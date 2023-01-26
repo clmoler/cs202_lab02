49f0f558ed122f3bee97b256ce1c4c4a17029738
ExistentialLamp <89541058+ExistentialLamp@users.noreply.github.com>
2023-01-26 11:07:15 -0900

    Create SchoolClass.h

---
+++ SchoolClass.h
@@ -0,0 +1,14 @@
+class SchoolClass
+{
+public:
+        bool partOfMajor(Student person)
+        bool partOfMinor(Student person)
+        int timeUntil(Date currentTime)
+private:
+        string _name;
+        int _time;
+        int _days;
+        int _credits;
+        string _subject;
+        string _building;
+}
