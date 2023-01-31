50e24b790264a231fea69955dee3d264fdbdc879
ExistentialLamp <sabosse@alaska.edu>
2023-01-31 09:56:19 -0900

    basic main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+#include "SchoolClass.h"
 
 int main()
 {
+	SchoolClass cs202("Computer Science 202", [0, 945, 0, 945, 0], [0, 1, 0, 1, 0], 3, "Computer Science", "Duckering");
+	std::cout << cs202.getName() << std::endl << cs202.getTime() << std::endl
+		<< cs202.getDays() << std::endl << cs202.getCredits() << std::endl
+		<< cs202.getBuilding() << std::endl;
+	if(cs202.partOfMajor("Math"))
+	{
+		std::cout << "Part of Major (Math): True"
+	}
+	else
+	{
+		std::cout << "Part of Major (Math): False"
+	}
 	return 0;
 }
